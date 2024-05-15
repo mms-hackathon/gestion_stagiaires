ef26d7c20de5d7c883b4d5b9eca0d857965d0ccb
zikoo <sidqui.zakaria1@gmail.com>
2024-05-15 03:36:50 +0100

    complete page login an php

--- pages/servers.php
+++ pages/servers.php
@@ -1,3 +1,19 @@
-<?php
-$nom = $_POST['name'];
-echo "smiyti"." ".$nom;
+<?php 
+
+require_once "db.php";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$userType= $_POST['userType'];
+$query = "SELECT * FROM $userType WHERE email = :email AND password = :password ";
+
+$statement = $pdo->prepare($query); 
+
+$statement->execute([
+    ':email'=>$email,
+    ':password'=>$password
+]);
+
+$response = $statement->fetch(PDO::FETCH_ASSOC);
+
+var_dump($response);
